9ebad3edf6317c4c438e61423afaead48eda5af8
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-09-19 17:29:41 +0530

    Add files via upload

---
+++ Edimux.sh
@@ -0,0 +1,59 @@
+echo -e "\e[1m\e[31m\
+
+                         ________  _____  _____ ___   _
+                        ||   || \\ ||||\\//|||| ||\\ //
+                        ||== ||  ))|||| \/ |||| || )X(
+                        ||___||_// ||||    ||\\_//// \\ 
+echo " "
+
+echo " "
+echo ""
+sleep 8.0
+pkg install mpv -y
+termux-setup-storage
+cd  /data/data/com.termux/files/usr/etc
+
+rm bash.bashrc
+
+
+
+cd $HOME
+
+
+
+cd Edimux
+
+
+
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+
+
+
+cd $HOME
+
+
+
+cd T-LOAD
+
+
+clear
+bash packages.sh
+clear
+sleep 4.0
+bash ins.sh
+sleep 5.0
+clear
+sleep 5.0
+bash thanks.sh
+sleep 2.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "             "EXIT FROM TERMUX AND RE OPEN IT AFTER 5 SECONDS |lolcat
+echo " "
+echo " "
+sleep 3.0
+cd $HOME
---
+++ bash.bashrc
@@ -0,0 +1,5 @@
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+PS1='\033[1;91mLavSarkari[\033[1;93m\W\033[1;91m]:➡️ \033[1;92m'
+clear
